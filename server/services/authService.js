77d3bfe5ec3e848776e1c80af78b48f599225a7b
shreya <shedgeshreyy03@gmail.com>
2024-07-31 15:43:44 +0530

    committ

--- server/services/authService.js
+++ server/services/authService.js
@@ -4,12 +4,14 @@ const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToekn = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhone = process.env.TWILIO_PHONE_NO;
+// const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 
 const twilioClient = new twilio(accountSid, authToekn);
 
 const sendPhoneOTP = asyncHandler(async (phoneNo, otp) => {
   const sendOtp = await twilioClient.messages.create({
     body: `Hi! Your otp is: ${otp}`,
+    // messagingServiceSid: messagingServiceSid,
     to: phoneNo,
     from: twilioPhone,
   });
