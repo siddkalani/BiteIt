4fe3328637ada3ef4b6a26aad33cd6ce51310b4a
shreya <shedgeshreyy03@gmail.com>
2024-08-10 02:17:59 +0530

    commit

--- client/navigation.js
+++ client/navigation.js
@@ -9,6 +9,7 @@ import OTP from "./screens/UserAuth/Otp";
 import NewUser from "./screens/UserAuth/NewUser";
 import FoodItem from "./screens/Main/FoodItem/FoodItem";
 import AdminHome from "./screens/Admin/AdminHome";
+import SearchResults from "./screens/Main/SearchBar/SearchResults";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,12 +17,13 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="intro" component={Intro1} />
+        {/* <Stack.Screen name="intro" component={Intro1} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="Otp" component={OTP} />
         <Stack.Screen name="AdminHome" component={AdminHome} />
-        <Stack.Screen name="NewUser" component={NewUser} />
+        <Stack.Screen name="NewUser" component={NewUser} /> */}
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SearchResults" component={SearchResults} />
         <Stack.Screen name="FoodItem" component={FoodItem} />
       </Stack.Navigator>
     </NavigationContainer>
