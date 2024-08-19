1d12a6da9d93099b743a32408602eea0568a5216
shreya <shedgeshreyy03@gmail.com>
2024-08-20 00:45:44 +0530

    cart

--- client/screens/Main/Home/SearchModal.js
+++ client/screens/Main/Home/SearchModal.js
@@ -17,8 +17,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "@env";
 import axios from "axios";
 import { fetchcategory } from "../../../store/Slices/categorySlice";
-import SearchResults from './../SearchBar/SearchResults';
-import SearchCategories from "./../SearchBar/SearchCategories"; 
+import SearchResults from "./../SearchBar/SearchResults";
+import SearchCategories from "./../SearchBar/SearchCategories";
 import SearchHistory from "./../SearchBar/SearchHistory";
 import { FontFamily, FontSize } from "../../../GlobalStyles";
 import Header from "../../../components/Layout/BaseHeader"; // Import the new SearchHeader component
@@ -103,7 +103,6 @@ const SearchModal = ({ isModalVisible, closeSearchModal }) => {
               }}
             >
               <View className="bg-white px-4 py-2 flex-1 rounded-none">
-                
                 {/* Use the separated SearchHeader component */}
                 <Header closeSearchModal={closeSearchModal} />
 
@@ -122,10 +121,10 @@ const SearchModal = ({ isModalVisible, closeSearchModal }) => {
                   isLoading ? (
                     <ActivityIndicator size="large" color="#0000ff" />
                   ) : (
-                      <SearchResults
-                        closeSearchModal={closeSearchModal}
-                        searchResults={searchResults}
-                        updateSearchHistory={updateSearchHistory}
+                    <SearchResults
+                      closeSearchModal={closeSearchModal}
+                      searchResults={searchResults}
+                      updateSearchHistory={updateSearchHistory}
                       onSelectItem={handleItemSelect} // Pass the item select handler
                     />
                   )
@@ -149,232 +148,3 @@ const SearchModal = ({ isModalVisible, closeSearchModal }) => {
 };
 
 export default SearchModal;
-
-
-
-// import React, { useState, useEffect } from "react";
-// import {
-//   View,
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-//   Modal,
-//   KeyboardAvoidingView,
-//   Platform,
-//   TouchableWithoutFeedback,
-//   Keyboard,
-//   FlatList,
-//   Image,
-// } from "react-native";
-// import { SafeAreaView } from "react-native";
-// import * as Icon from "react-native-feather";
-// import { FontFamily, FontSize } from "../../../GlobalStyles";
-// import Ionicons from "react-native-vector-icons/Ionicons";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchcategory } from "../../../store/Slices/categorySlice";
-// import { BASE_URL } from "@env";
-
-// const initialSearchHistory = [
-//   {
-//     name: "Pizza",
-//     image: require("../../../assets/images/categories/icecream.png"),
-//   },
-//   {
-//     name: "Burgers",
-//     image: require("../../../assets/images/categories/icecream.png"),
-//   },
-//   {
-//     name: "Sushi",
-//     image: require("../../../assets/images/categories/icecream.png"),
-//   },
-//   {
-//     name: "Pasta",
-//     image: require("../../../assets/images/categories/icecream.png"),
-//   },
-// ];
-
-// const SearchModal = ({ isModalVisible, closeSearchModal }) => {
-//   const dispatch = useDispatch();
-
-//   const categories = useSelector((state) => state.category.items);
-//   const categoryStatus = useSelector((state) => state.category.status);
-//   const categoryError = useSelector((state) => state.category.error);
-
-//   useEffect(() => {
-//     if (categoryStatus === "idle") {
-//       dispatch(fetchcategory());
-//     }
-//   }, [categoryStatus, dispatch]);
-
-//   const [searchHistory, setSearchHistory] = useState(initialSearchHistory);
-
-//   const removeSearchItem = (item) => {
-//     setSearchHistory(
-//       searchHistory.filter((historyItem) => historyItem.name !== item)
-//     );
-//   };
-
-//   return (
-//     <Modal
-//       visible={isModalVisible}
-//       animationType="slide"
-//       transparent={true}
-//       onRequestClose={closeSearchModal}
-//     >
-//       <KeyboardAvoidingView
-//         style={{ flex: 1 }}
-//         behavior={Platform.OS === "ios" ? "padding" : "height"}
-//       >
-//         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-//           <View className="flex-1 justify-start bg-black bg-opacity-50">
-//             <SafeAreaView
-//               style={{
-//                 flex: 1,
-//                 backgroundColor: "white",
-//                 overflow: "hidden",
-//               }}
-//             >
-//               <View className="bg-white px-4 py-2 flex-1 rounded-none">
-//                 <View className="flex-row items-center space-x-4">
-//                   <TouchableOpacity
-//                     onPress={closeSearchModal}
-//                     className="w-10 h-10  justify-center absolute"
-//                   >
-//                     <Ionicons name="arrow-back" size={24} color="black" />
-//                   </TouchableOpacity>
-//                   <View className="flex-1 items-center">
-//                     <Text
-//                       style={{
-//                         fontFamily: FontFamily.poppinsMedium,
-//                         fontSize: FontSize.size_lg,
-//                         lineHeight: 28,
-//                       }}
-//                       className="text-black"
-//                     >
-//                       Search for dishes
-//                     </Text>
-//                   </View>
-//                 </View>
-
-//                 <View className="flex-row bg-[#F4F5F9] items-center p-3 rounded-lg mt-4">
-//                   <Icon.Search height="20" width="20" stroke="gray" />
-//                   <TextInput
-//                     placeholder="What are you craving?"
-//                     className="flex-1 ml-2"
-//                     autoFocus
-//                   />
-//                 </View>
-
-//                 {/* Recent Searches */}
-//                 <View className="mt-4">
-//                   <Text
-//                     style={{
-//                       fontFamily: FontFamily.poppinsSemiBold,
-//                       fontSize: FontSize.size_md,
-//                     }}
-//                   >
-//                     Recent Searches
-//                   </Text>
-//                   <FlatList
-//                     data={searchHistory}
-//                     renderItem={({ item }) => (
-//                       <View
-//                         className="bg-white p-4 mt-2 rounded-lg shadow flex-row justify-between items-center"
-//                         style={{
-//                           width: "98%",
-//                           alignSelf: "center",
-//                           shadowColor: "#000",
-//                           shadowOffset: { width: 0, height: 2 },
-//                           shadowOpacity: 0.1,
-//                           shadowRadius: 5,
-//                           elevation: 3,
-//                         }}
-//                       >
-//                         <View className="flex-row items-center">
-//                           <Image
-//                             source={item.image}
-//                             className="w-7 h-7 rounded-lg mr-3"
-//                             style={{ resizeMode: "contain" }}
-//                           />
-//                           <Text
-//                             style={{
-//                               fontFamily: FontFamily.poppinsMedium,
-//                               fontSize: FontSize.size_md,
-//                             }}
-//                           >
-//                             {item.name}
-//                           </Text>
-//                         </View>
-//                         <TouchableOpacity
-//                           onPress={() => removeSearchItem(item.name)}
-//                         >
-//                           <Icon.X width={20} height={20} stroke="red" />
-//                         </TouchableOpacity>
-//                       </View>
-//                     )}
-//                     keyExtractor={(item, index) => index.toString()}
-//                     contentContainerStyle={{ paddingBottom: 10 }}
-//                   />
-//                 </View>
-
-//                 {/* Categories */}
-//                 <View className="mt-4">
-//                   <Text
-//                     style={{
-//                       fontFamily: FontFamily.poppinsSemiBold,
-//                       fontSize: FontSize.size_md,
-//                     }}
-//                   >
-//                     Categories
-//                   </Text>
-//                   <FlatList
-//                     data={categories}
-//                     renderItem={({ item }) => (
-//                       <View
-//                         className="w-1/3 p-2"
-//                         style={{
-//                           alignItems: "center",
-//                         }}
-//                       >
-//                         <View
-//                           className="bg-white rounded-lg shadow w-full py-1"
-//                           style={{
-//                             alignItems: "center",
-//                             shadowColor: "#000",
-//                             shadowOffset: { width: 0, height: 2 },
-//                             shadowOpacity: 0.1,
-//                             shadowRadius: 5,
-//                             elevation: 3,
-//                           }}
-//                         >
-//                           <Image
-//                             source={{ uri:`${BASE_URL}/uploads/${item.image}` }} // Assuming item.image is a URL
-//                             className="w-16 h-16 rounded-lg"
-//                             style={{ resizeMode: "cover" }}
-//                           />
-//                           <Text
-//                             style={{
-//                               fontFamily: FontFamily.poppinsMedium,
-//                               fontSize: FontSize.size_md,
-//                             }}
-//                           >
-//                             {item.categoryName}
-//                           </Text>
-//                         </View>
-//                       </View>
-//                     )}
-//                     keyExtractor={(item, index) => index.toString()}
-//                     numColumns={3}
-//                     contentContainerStyle={{ paddingBottom: 10 }}
-//                   />
-//                 </View>
-//               </View>
-//             </SafeAreaView>
-//           </View>
-//         </TouchableWithoutFeedback>
-//       </KeyboardAvoidingView>
-//     </Modal>
-//   );
-// };
-
-// export default SearchModal;
--- client/screens/Main/SearchBar/SearchResults.js
+++ client/screens/Main/SearchBar/SearchResults.js
@@ -1,71 +1,226 @@
+// import React from "react";
+// import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+// import { FontFamily, FontSize } from "../../../GlobalStyles";
+// import { BASE_URL } from "@env";
+// import { useNavigation } from "@react-navigation/native";
+
+// const SearchResults = ({ searchResults, closeSearchModal, updateSearchHistory }) => {
+//   const navigation = useNavigation();
+
+//   const handleAddToCartPress = (item) => {
+//     // Close the modal
+//     // closeSearchModal();
+
+//     // Navigate to FoodItem page
+//     // navigation.navigate("FoodItem", { foodItem: item });
+
+//     // Update search history
+//     updateSearchHistory(item);
+//   };
+
+//   const renderItem = ({ item }) => (
+//     <View
+//       className="bg-white p-4 mt-2 rounded-lg shadow flex-row justify-between items-center"
+//       style={{ flexDirection: "row", marginVertical: 8 }}
+//     >
+//       <View className="flex-row items-center">
+//         <Image
+//           source={{ uri: `${BASE_URL}/items_uploads/${item.image}` }}
+//           className="w-16 h-16 rounded-lg mr-3"
+//           style={{ resizeMode: "contain" }}
+//         />
+//         <View>
+//           <Text
+//             style={{
+//               fontFamily: FontFamily.poppinsMedium,
+//               fontSize: FontSize.size_md,
+//             }}
+//           >
+//             {item.itemName}
+//           </Text>
+//           <Text
+//             style={{
+//               fontFamily: FontFamily.poppinsRegular,
+//               fontSize: FontSize.size_sm,
+//               color: "#888", // Light grey color for the price text
+//             }}
+//           >
+//             ${item.itemPrice}
+//           </Text>
+//         </View>
+//       </View>
+//       <TouchableOpacity
+//         className="bg-blue-500 p-2 rounded-lg"
+//         style={{ marginLeft: 10 }}
+//         onPress={() => handleAddToCartPress(item)}
+//       >
+//         <Text
+//           style={{
+//             color: "white",
+//             fontFamily: FontFamily.poppinsMedium,
+//             fontSize: FontSize.size_sm,
+//           }}
+//         >
+//           Add to Cart
+//         </Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+
+//   return (
+//     <View className="flex-1">
+//       {searchResults.length === 0 ? (
+//         <Text
+//           style={{
+//             fontFamily: FontFamily.poppinsRegular,
+//             fontSize: FontSize.size_md,
+//             color: "#888", // Light grey color for the "No items found" text
+//             textAlign: "center",
+//             marginTop: 20,
+//           }}
+//         >
+//           No items found
+//         </Text>
+//       ) : (
+//         <FlatList
+//           data={searchResults}
+//           renderItem={renderItem}
+//           keyExtractor={(item, index) => index.toString()}
+//           contentContainerStyle={{ paddingBottom: 10 }}
+//         />
+//       )}
+//     </View>
+//   );
+// };
+
+// export default SearchResults;
+
+
 import React from "react";
 import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import { FontFamily, FontSize } from "../../../GlobalStyles";
 import { BASE_URL } from "@env";
 import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, updateCartQuantity, removeFromCart } from "../../../store/Slices/cartSlice";
+import * as Icon from "react-native-feather";
+import { LinearGradient } from "expo-linear-gradient";
 
 const SearchResults = ({ searchResults, closeSearchModal, updateSearchHistory }) => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart);
 
   const handleAddToCartPress = (item) => {
-    // Close the modal
-    // closeSearchModal();
+    const existingItem = cartItems.find(cartItem => cartItem._id === item._id);
 
-    // Navigate to FoodItem page
-    // navigation.navigate("FoodItem", { foodItem: item });
+    if (existingItem) {
+      dispatch(updateCartQuantity({ itemId: item._id, quantity: existingItem.quantity + 1 }));
+    } else {
+      dispatch(addToCart({ ...item, quantity: 1 }));
+    }
 
-    // Update search history
     updateSearchHistory(item);
+    // closeSearchModal();
   };
 
-  const renderItem = ({ item }) => (
-    <View
-      className="bg-white p-4 mt-2 rounded-lg shadow flex-row justify-between items-center"
-      style={{ flexDirection: "row", marginVertical: 8 }}
-    >
-      <View className="flex-row items-center">
-        <Image
-          source={{ uri: `${BASE_URL}/items_uploads/${item.image}` }}
-          className="w-16 h-16 rounded-lg mr-3"
-          style={{ resizeMode: "contain" }}
-        />
-        <View>
-          <Text
-            style={{
-              fontFamily: FontFamily.poppinsMedium,
-              fontSize: FontSize.size_md,
-            }}
-          >
-            {item.itemName}
-          </Text>
-          <Text
-            style={{
-              fontFamily: FontFamily.poppinsRegular,
-              fontSize: FontSize.size_sm,
-              color: "#888", // Light grey color for the price text
-            }}
-          >
-            ${item.itemPrice}
-          </Text>
-        </View>
-      </View>
-      <TouchableOpacity
-        className="bg-blue-500 p-2 rounded-lg"
-        style={{ marginLeft: 10 }}
-        onPress={() => handleAddToCartPress(item)}
+  const handleIncrement = (itemId) => {
+    const item = cartItems.find(cartItem => cartItem._id === itemId);
+    if (item) {
+      dispatch(updateCartQuantity({ itemId, quantity: item.quantity + 1 }));
+    }
+  };
+
+  const handleDecrement = (itemId) => {
+    const item = cartItems.find(cartItem => cartItem._id === itemId);
+    if (item && item.quantity > 1) {
+      dispatch(updateCartQuantity({ itemId, quantity: item.quantity - 1 }));
+    } else if (item && item.quantity === 1) {
+      dispatch(removeFromCart({ itemId }));
+    }
+  };
+
+  const renderItem = ({ item }) => {
+    const existingItem = cartItems.find(cartItem => cartItem._id === item._id);
+
+    return (
+      <View
+        className="bg-white p-4 mt-2 rounded-lg shadow flex-row justify-between items-center"
+        style={{ flexDirection: "row", marginVertical: 8 }}
       >
-        <Text
-          style={{
-            color: "white",
-            fontFamily: FontFamily.poppinsMedium,
-            fontSize: FontSize.size_sm,
-          }}
-        >
-          Add to Cart
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+        <View className="flex-row items-center">
+          <Image
+            source={{ uri: `${BASE_URL}/items_uploads/${item.image}` }}
+            className="w-16 h-16 rounded-lg mr-3"
+            style={{ resizeMode: "contain" }}
+          />
+          <View>
+            <Text
+              style={{
+                fontFamily: FontFamily.poppinsMedium,
+                fontSize: FontSize.size_md,
+              }}
+            >
+              {item.itemName}
+            </Text>
+            <Text
+              style={{
+                fontFamily: FontFamily.poppinsRegular,
+                fontSize: FontSize.size_sm,
+                color: "#888",
+              }}
+            >
+              ${item.itemPrice}
+            </Text>
+          </View>
+        </View>
+        {existingItem ? (
+          <View className="flex-row items-center space-x-2">
+            <TouchableOpacity
+              onPress={() => handleDecrement(item._id)}
+              className="p-2 bg-gray-200 rounded-full"
+              style={{ width: 30, height: 30, justifyContent: 'center', alignItems: 'center' }}
+            >
+              <Icon.Minus width={16} height={16} stroke="black" />
+            </TouchableOpacity>
+            <Text>{existingItem.quantity}</Text>
+            <TouchableOpacity
+              onPress={() => handleIncrement(item._id)}
+              className="p-2 bg-gray-200 rounded-full"
+              style={{ width: 30, height: 30, justifyContent: 'center', alignItems: 'center' }}
+            >
+              <Icon.Plus width={16} height={16} stroke="black" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <LinearGradient
+            colors={["#007022", "#54d17a", "#bcffd0"]}
+            start={{ x: 0, y: 1 }}
+            end={{ x: 1.9, y: 0 }}
+            className="rounded-md"
+            style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+          >
+            <TouchableOpacity
+              onPress={() => handleAddToCartPress(item)}
+              className="flex-row items-center justify-center"
+              style={{ borderRadius: 5 }}
+            >
+              <Icon.ShoppingBag width={15} height={15} stroke="white" />
+              <Text
+                className="text-white ml-2"
+                style={{
+                  fontFamily: FontFamily.poppinsMedium,
+                  fontSize: FontSize.size_sm,
+                }}
+              >
+                Add to Cart
+              </Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        )}
+      </View>
+    );
+  };
 
   return (
     <View className="flex-1">
@@ -74,7 +229,7 @@ const SearchResults = ({ searchResults, closeSearchModal, updateSearchHistory })
           style={{
             fontFamily: FontFamily.poppinsRegular,
             fontSize: FontSize.size_md,
-            color: "#888", // Light grey color for the "No items found" text
+            color: "#888",
             textAlign: "center",
             marginTop: 20,
           }}
@@ -85,7 +240,7 @@ const SearchResults = ({ searchResults, closeSearchModal, updateSearchHistory })
         <FlatList
           data={searchResults}
           renderItem={renderItem}
-          keyExtractor={(item, index) => index.toString()}
+          keyExtractor={(item) => item._id.toString()}
           contentContainerStyle={{ paddingBottom: 10 }}
         />
       )}
