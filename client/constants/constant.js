98c07ba2c6dd804b3c5f63bcbed5061afb2f1ab8
siddkalani <siddharthkalani6@gmail.com>
2024-12-12 10:40:39 +0530

    build

--- client/constants/constant.js
+++ client/constants/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://192.168.29.147:3000";
+export const BASE_URL = "https://canteenapp-v5h1.onrender.com";
 // export const BASE_URL = 'http://localhost:3000';
 // export const BASE_URL = "http://127.0.0.1:3000";
 
